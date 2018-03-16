356e34835ad41dfcd2faa323d4037825d84cafa6
Esben Vial <cjs614@hum.ku.dk>
2018-03-16 19:33:14 +0100

    fix path

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,26 +5,28 @@ class zabbix_agent_haproxy (
 
   file { 'userparameter_haproxy.conf':
     path   => "${agent_include}/userparameter_haproxy.conf",
-    source => 'puppet:///modules/zabbix_agent_haproxy/userparameter_haproxy.conf',
+    source => "puppet:///modules/${module_name}/userparameter_haproxy.conf",
   }
 
   file { 'haproxy_discovery.sh':
     path   => "${bin_path}/haproxy_discovery.sh",
-    source => 'puppet:///modules/zabbix_agent_haproxy/haproxy_discovery.sh',
+    source => "puppet:///modules/${module_name}/haproxy_discovery.sh",
     mode   => '0775',
   }
 
   file { 'haproxy_stats.sh':
     path   => "${bin_path}/haproxy_stats.sh",
-    source => 'puppet:///modules/zabbix_agent_haproxy/haproxy_stats.sh',
+    source => "puppet:///modules/${module_name}/haproxy_stats.sh",
     mode   => '0775',
   }
 
   package { 'netcat-openbsd': ensure => 'present', }
+  package { 'socat': ensure => 'present', }
 
   exec {'zabbix haproxy membership':
     unless  => "/bin/grep -q 'haproxy\\S*zabbix' /etc/group",
-    command => '/sbin/usermod -aG haproxy zabbix',
+    command => '/usr/sbin/usermod -aG haproxy zabbix',
     notify  => Service['zabbix-agent'],
   }
+
 }
