3c79811f45df8b9ffcc7b5e93f759ba2b0136755
Esben Vial <cjs614@hum.ku.dk>
2018-03-16 13:53:53 +0100

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+class zabbix_agent_haproxy (
+  $agent_include  = '/etc/zabbix/zabbix_agentd.d/',
+  $bin_path       = '/usr/local/bin',
+  ) {
+
+  file { 'userparameter_haproxy.conf':
+    path   => "${agent_include}/userparameter_haproxy.conf",
+    source => 'puppet:///modules/zabbix_agent_haproxy/userparameter_haproxy.conf',
+  }
+
+  file { 'haproxy_discovery.sh':
+    path   => "${bin_path}/haproxy_discovery.sh",
+    source => 'puppet:///modules/zabbix_agent_haproxy/haproxy_discovery.sh',
+    mode   => '0775',
+  }
+
+  file { 'haproxy_stats.sh':
+    path   => "${bin_path}/haproxy_stats.sh",
+    source => 'puppet:///modules/zabbix_agent_haproxy/haproxy_stats.sh',
+    mode   => '0775',
+  }
+
+  package { 'netcat-openbsd': ensure => 'present', }
+
+  exec {'zabbix haproxy membership':
+    unless  => "grep -q 'haproxy\\S*zabbix' /etc/group",
+    command => 'usermod -aG haproxy zabbix',
+  }
+}
